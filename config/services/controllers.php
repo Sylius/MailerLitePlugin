18044f91d4046eee232b736f86f6db938694543b
Rafikooo <thecrazyrafik@gmail.com>
2026-01-16 12:39:34 +0100

    Add newsletter subscription CTA on thank you page
    - Add prominent newsletter subscription card on order thank you page
    - Create NewsletterSubscriptionController for handling subscriptions
    - Use Twig hooks to inject newsletter component
    - Mark customer as subscribed and sync to MailerLite
    - Add translations for UI and flash messages
    
    Changed approach from checkout form extension to thank you page
    for better UX - user is not overwhelmed during checkout process.

--- config/services/controllers.php
+++ config/services/controllers.php
@@ -13,20 +13,22 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-use Sylius\MailerLitePlugin\EventListener\CustomerRegistrationListener;
+use Sylius\MailerLitePlugin\Controller\NewsletterSubscriptionController;
 use Sylius\MailerLitePlugin\Subscriber\NewsletterSubscriberInterface;
 
 return static function (ContainerConfigurator $container): void {
     $services = $container->services();
 
     $services
-        ->set('sylius_mailerlite.listener.customer_registration', CustomerRegistrationListener::class)
+        ->set('sylius_mailerlite.controller.newsletter_subscription', NewsletterSubscriptionController::class)
         ->args([
+            service('sylius.repository.order'),
             service(NewsletterSubscriberInterface::class),
+            service('doctrine.orm.entity_manager'),
+            service('router'),
+            service('security.csrf.token_manager'),
+            service('translator'),
         ])
-        ->tag('kernel.event_listener', [
-            'event' => 'sylius.customer.post_register',
-            'method' => 'onCustomerPostRegister',
-        ])
+        ->public()
     ;
 };
---
+++ src/Controller/NewsletterSubscriptionController.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\MailerLitePlugin\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Sylius\Component\Core\Model\CustomerInterface;
+use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Core\Repository\OrderRepositoryInterface;
+use Sylius\MailerLitePlugin\Subscriber\NewsletterSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Csrf\CsrfToken;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+final class NewsletterSubscriptionController
+{
+    public function __construct(
+        private readonly OrderRepositoryInterface $orderRepository,
+        private readonly NewsletterSubscriberInterface $newsletterSubscriber,
+        private readonly EntityManagerInterface $entityManager,
+        private readonly UrlGeneratorInterface $urlGenerator,
+        private readonly CsrfTokenManagerInterface $csrfTokenManager,
+        private readonly TranslatorInterface $translator,
+    ) {
+    }
+
+    public function subscribeAction(Request $request, string $orderToken): Response
+    {
+        $token = new CsrfToken('newsletter_subscribe', $request->request->get('_token'));
+
+        if (!$this->csrfTokenManager->isTokenValid($token)) {
+            throw new NotFoundHttpException();
+        }
+
+        /** @var OrderInterface|null $order */
+        $order = $this->orderRepository->findOneByTokenValue($orderToken);
+
+        if ($order === null) {
+            throw new NotFoundHttpException();
+        }
+
+        $customer = $order->getCustomer();
+
+        if (!$customer instanceof CustomerInterface) {
+            throw new NotFoundHttpException();
+        }
+
+        $customer->setSubscribedToNewsletter(true);
+        $this->entityManager->flush();
+
+        $this->newsletterSubscriber->subscribe($customer);
+
+        /** @var FlashBagInterface $flashBag */
+        $flashBag = $request->getSession()->getBag('flashes');
+        $flashBag->add('success', $this->translator->trans('sylius_mailerlite.newsletter_subscribed'));
+
+        return new RedirectResponse(
+            $this->urlGenerator->generate('sylius_shop_order_thank_you'),
+        );
+    }
+}
